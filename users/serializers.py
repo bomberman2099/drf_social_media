67571e90e89c0e144b2762329d0a24706eec11df
amirali <ososgol@gmail.com>
2025-09-26 23:59:20 +0330

    feat(users): add liked_posts property

--- users/serializers.py
+++ users/serializers.py
@@ -3,8 +3,10 @@ from .models import Post, MyUser, ProfileUser
 
 
 class PostSerializer(serializers.ModelSerializer):
-
+    liked_posts = serializers.SerializerMethodField()
     class Meta:
         model = Post
         fields = '__all__'
 
+    def get_liked_posts(self, obj):
+        return obj.liked_posts
