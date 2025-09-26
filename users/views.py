ae6720803327f7f041ef53b5ac7ebe63dc79ea6c
amirali <ososgol@gmail.com>
2025-09-26 23:59:54 +0330

    feat(users): add post list json_response view

--- users/views.py
+++ users/views.py
@@ -1,5 +1,13 @@
-
+from django.http import JsonResponse
 from django.shortcuts import render
 
+from users.models import Post
+from users.serializers import PostSerializer
 
+
+# Create your views here.
+def post_list(request):
+    posts = Post.objects.all()
+    serializer = PostSerializer(posts, many=True)
+    return JsonResponse(data=serializer.data, safe=False)
 
