3568a7c2f3553d9a48c7b339dc8424c570f7c8b2
amirali <ososgol@gmail.com>
2025-09-27 12:50:44 +0330

    feat(users): add ApiVIew PostList

--- users/views.py
+++ users/views.py
@@ -1,13 +1,35 @@
 from django.http import JsonResponse
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from users.models import Post
 from users.serializers import PostSerializer
 
 
 # Create your views here.
-def post_list(request):
-    posts = Post.objects.all()
-    serializer = PostSerializer(posts, many=True)
-    return JsonResponse(data=serializer.data, safe=False)
+# def post_list(request):
+#     posts = Post.objects.all()
+#     serializer = PostSerializer(posts, many=True)
+#     return JsonResponse(data=serializer.data, safe=False)
+
+
+class PostList(APIView):
+    serializer_class = PostSerializer
+    
+    def get(self, request, format=None):
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = PostSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
