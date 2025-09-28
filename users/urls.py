cfb3532e8384055404e99a8cda50f22736d7d2f1
amirali <ososgol@gmail.com>
2025-09-28 23:33:59 +0330

    feat(users): add logout API view with JWT blacklist

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from users import views
-
+from rest_framework_simplejwt import views as jwt_views
 urlpatterns = [
     path('posts/', views.PostList.as_view(), name='post_list'),
-
+    path('posts/<int:pk>/', views.PostDetail.as_view(), name='post_detail'),
+    path('signup/', views.SignUpVIew.as_view(), name='signup'),
+    path('login/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token-refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
 ]
